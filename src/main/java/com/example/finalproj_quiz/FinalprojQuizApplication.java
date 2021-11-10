3ead0c6c796c14987d1245efc9c51921e21a5c6e
cecinordahl <cecilie.nordahl@me.com>
2021-11-10 16:59:55 +0100

    Initial commit

---
+++ src/main/java/com/example/finalproj_quiz/FinalprojQuizApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalproj_quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalprojQuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalprojQuizApplication.class, args);
+    }
+
+}
