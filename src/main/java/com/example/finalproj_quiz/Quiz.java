c82336b5c46b714a13668d16a8c319f27f0adacf
cecinordahl <cecilie.nordahl@me.com>
2021-11-12 10:50:42 +0100

    Lagt til Player + Quiz klasse og controller

---
+++ src/main/java/com/example/finalproj_quiz/Quiz.java
@@ -0,0 +1,61 @@
+package com.example.finalproj_quiz;
+
+public class Quiz {
+    private String category;
+    private String correctAnswer;
+    private long id;
+    private String[] incorrectAnswers;
+    private String question;
+    private String type;
+
+    public Quiz(){}
+
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String[] getIncorrectAnswers() {
+        return incorrectAnswers;
+    }
+
+    public void setIncorrectAnswers(String[] incorrectAnswers) {
+        this.incorrectAnswers = incorrectAnswers;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
---
+++ src/main/java/com/example/finalproj_quiz/QuizController.java
@@ -0,0 +1,46 @@
+package com.example.finalproj_quiz;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+
+
+
+@Controller
+public class QuizController {
+
+
+    @Autowired
+    ObjectMapper mapper;
+
+
+
+    @GetMapping("/")
+    public String showOneQuestion(Model model) throws JsonProcessingException {
+        Quiz[] quiz = getQuiz();
+
+        model.addAttribute("entireQuiz", mapper.writeValueAsString(quiz));
+        model.addAttribute("question", mapper.writeValueAsString(quiz[0].getQuestion()));
+
+        model.addAttribute("A", mapper.writeValueAsString(quiz[0].getCorrectAnswer()));
+        model.addAttribute("B", mapper.writeValueAsString(quiz[0].getIncorrectAnswers()[0]));
+        model.addAttribute("C", mapper.writeValueAsString(quiz[0].getIncorrectAnswers()[1]));
+        model.addAttribute("D", mapper.writeValueAsString(quiz[0].getIncorrectAnswers()[2]));
+
+        return "play_all";
+    }
+
+
+    /* Henter ut quiz fra url, returnerer som array */
+    public Quiz[] getQuiz(){
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Quiz[]> quizzes = restTemplate.getForEntity("https://api.trivia.willfry.co.uk/questions?limit=10", Quiz[].class);
+        return quizzes.getBody();
+    }
+}
