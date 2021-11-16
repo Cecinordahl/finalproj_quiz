6cde85782eca35c4a7e35f49003f3b7eea77cd28
Aasgrav <88495565+aasgrav@users.noreply.github.com>
2021-11-16 10:00:54 +0100

    Try to fix question_page and submitting answer

--- src/main/java/com/example/finalproj_quiz/QuestionsController.java
+++ src/main/java/com/example/finalproj_quiz/QuestionsController.java
@@ -132,7 +132,7 @@ public class QuestionsController {
     }
 
     @PostMapping("/play/{quizCode}/{questionNumber}")
-    public String postScore(@PathVariable int quizCode, @PathVariable int questionNumber, HttpSession session, Model model){
+    public String postScore(@PathVariable int quizCode, @PathVariable int questionNumber, HttpSession session, Model model, @RequestParam String answer){
         model.addAttribute("player", session.getAttribute("player"));
 
         if (isFinalQuestion){
@@ -146,7 +146,6 @@ public class QuestionsController {
     public String waitingPage(@PathVariable int quizCode, Model model, HttpSession session){
         model.addAttribute("scoreboard", scoreboard);
         model.addAttribute("player", session.getAttribute("player"));
-
         return "waiting_page";
     }
 
