55fa0daf3f4b178fc59caed29d26d764218488e7
cecinordahl <cecilie.nordahl@me.com>
2021-11-26 12:13:16 +0100

    m

--- src/main/java/com/example/finalproj_quiz/Game.java
+++ src/main/java/com/example/finalproj_quiz/Game.java
@@ -33,15 +33,18 @@ public class Game {
         return ThreadLocalRandom.current().nextInt(1, 1000);
     }
 
+
+
+
     // function to check if all players have been forwarded to the next question
     public synchronized void addPlayerForwardAndCheckPlayerCounter(Player currentPlayer) {
         this.playerForwarded.add(currentPlayer.getName());
         int playerCounter = this.playerForwarded.size();
 
-        if (this.isRemote && playerCounter == this.listOfPlayers.size() - 1) {
+        if (this.isRemote && (playerCounter == this.listOfPlayers.size() - 1)) {
             this.forwardPlayers = false;
             this.playerForwarded.clear();
-        } else if (!this.isRemote && playerCounter == this.listOfPlayers.size()) {
+        } else if (!this.isRemote && (playerCounter == this.listOfPlayers.size())) {
             this.forwardPlayers = false;
             this.playerForwarded.clear();
         }
